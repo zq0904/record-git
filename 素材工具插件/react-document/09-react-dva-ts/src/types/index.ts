92259ac71df462a7111bfcf5148cc91f9f601f1b
zhaoqi15 <zhaoqi15@58.com>
2020-10-14 20:52:24 +0800

    feat: 更新

--- 素材工具插件/react-document/09-react-dva-ts/src/types/index.ts
+++ 素材工具插件/react-document/09-react-dva-ts/src/types/index.ts
@@ -1,3 +1,12 @@
+import { GlobalNamespace, GlobalState } from './global'
+import { HomeNamespace, HomeState } from './home'
+import { FooNamespace, FooState } from './foo'
 export * from './global'
 export * from './home'
 export * from './foo'
+
+export type State = {
+  [GlobalNamespace]: GlobalState;
+  [HomeNamespace]: HomeState;
+  [FooNamespace]: FooState;
+}
--- 素材工具插件/react-document/09-react-dva-ts/src/views/Home/index.tsx
+++ 素材工具插件/react-document/09-react-dva-ts/src/views/Home/index.tsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import { connect } from 'dva'
+import React, { FC } from 'react'
+// import { connect } from 'dva'
+import { connect } from 'react-redux' // dva中使用react-redux@5.0.7版本
+import type { ConnectedProps } from 'react-redux' // @types/react-redux@7.1.9
 import { Link, routerRedux } from 'dva/router'
+import { State } from '../../types'
 
-const mapStateToProps = (state, ownProps) => ({
+// ownProps 默认会透传 类型需要处理下
+type OwnProps = {}
+
+const mapStateToProps = (state: State, ownProps: OwnProps) => ({
   homeState: state.Home
 })
 
 const connector = connect(mapStateToProps)
 
-const Home = ({ dispatch, homeState }) => {
+type PropsFromRedux = ConnectedProps<typeof connector> & OwnProps
+
+const Home: FC<PropsFromRedux> = ({ dispatch, homeState }) => {
 
   const handleJump = () => {
     // 由于dva使用的是 react-router-redux 强耦合导航跳转使用dispatch(action)
     dispatch(routerRedux.push({
       pathname: '/foo/456',
-      query: { // 这个query 虽然能在下一个页面拿到 但是不会依据tab的持久化策略 基本没用
-        b: 1
-      }
+      // query: { // 这个query 虽然能在下一个页面拿到 但是不会依据tab的持久化策略 基本没用
+      //   b: 1
+      // }
     }))
   }
 
